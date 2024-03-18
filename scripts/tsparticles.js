e51c93ee1e6c4395ac3d76028306a69116a1aa3d
Tanz <cauamelo2345@gmail.com>
2024-03-18 15:22:39 -0300

    Create tsparticles.js

---
+++ scripts/tsparticles.js
@@ -0,0 +1,123 @@
+$(document).ready(async function() {
+                await loadFull(tsParticles);
+
+                $("#tsparticles")
+                    .particles()
+                    .init({
+                            "fullScreen": {
+                                "enable": false
+                            },
+                            "particles": {
+                                "number": {
+                                    "value": 30,
+                                    "density": {
+                                        "enable": true,
+                                        "valueArea": 800
+                                    }
+                                },
+                                "color": {
+                                    "value": "#ffffff"
+                                },
+                                "shape": {
+                                    "type": "circle",
+                                    "stroke": {
+                                        "width": 0,
+                                        "color": "#000000"
+                                    },
+                                    "polygon": {
+                                        "nbSides": 5
+                                    },
+                                    "image": {
+                                        "src": "img/github.svg",
+                                        "width": 100,
+                                        "height": 100
+                                    }
+                                },
+                                "opacity": {
+                                    "value": 0.9,
+                                    "random": false,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 1,
+                                        "opacityMin": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "size": {
+                                    "value": 4,
+                                    "random": true,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 40,
+                                        "sizeMin": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "lineLinked": {
+                                    "enable": false,
+                                    "distance": 150,
+                                    "color": "#ffffff",
+                                    "opacity": 0.4,
+                                    "width": 1
+                                },
+                                "move": {
+                                    "enable": true,
+                                    "speed": 2,
+                                    "direction": "top",
+                                    "random": false,
+                                    "straight": false,
+                                    "outMode": "out",
+                                    "bounce": false,
+                                    "attract": {
+                                        "enable": false,
+                                        "rotateX": 600,
+                                        "rotateY": 1200
+                                    }
+                                }
+                            },
+                            "interactivity": {
+                                "detectOn": "canvas",
+                                "events": {
+                                    "onhover": {
+                                        "enable": false,
+                                        "mode": "grab"
+                                    },
+                                    "onclick": {
+                                        "enable": false,
+                                        "mode": "push"
+                                    },
+                                    "resize": true
+                                },
+                                "modes": {
+                                    "grab": {
+                                        "distance": 140,
+                                        "lineLinked": {
+                                            "opacity": 1
+                                        }
+                                    },
+                                    "bubble": {
+                                        "distance": 400,
+                                        "size": 40,
+                                        "duration": 2,
+                                        "opacity": 8,
+                                        "speed": 3
+                                    },
+                                    "repulse": {
+                                        "distance": 200,
+                                        "duration": 0.4
+                                    },
+                                    "push": {
+                                        "particlesNb": 4
+                                    },
+                                    "remove": {
+                                        "particlesNb": 2
+                                    }
+                                }
+                            },
+                            "retinaDetect": true
+                        },
+                        function(container) {
+                            // container is the particles container where you can play/pause or stop/start.
+                            // the container is already started, you don't need to start it manually.
+                        },
+                    );
